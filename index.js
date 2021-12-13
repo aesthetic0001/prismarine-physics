954ddca2061f8873223c82bb9df0f1e5a1232c3b
aesthetic <15858616+aesthetic0001@users.noreply.github.com>
2021-12-13 17:34:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,53 @@ const AABB = require('./lib/aabb')
 const math = require('./lib/math')
 const features = require('./lib/features')
 
+const physics = {
+  gravity: 0.08, // blocks/tick^2 https://minecraft.gamepedia.com/Entity#Motion_of_entities
+  airdrag: Math.fround(1 - 0.02), // actually (1 - drag)
+  yawSpeed: 3.0,
+  pitchSpeed: 3.0,
+  sprintSpeed: 1.3,
+  sneakSpeed: 0.3,
+  stepHeight: 0.6, // how much height can the bot step on without jump
+  negligeableVelocity: 0.003, // actually 0.005 for 1.8, but seems fine
+  soulsandSpeed: 0.4,
+  honeyblockSpeed: 0.4,
+  honeyblockJumpSpeed: 0.4,
+  ladderMaxSpeed: 0.15,
+  ladderClimbSpeed: 0.2,
+  playerHalfWidth: 0.3,
+  playerHeight: 1.8,
+  waterInertia: 0.8,
+  lavaInertia: 0.5,
+  liquidAcceleration: 0.02,
+  airborneInertia: 0.91,
+  airborneAcceleration: 0.02,
+  defaultSlipperiness: 0.6,
+  outOfLiquidImpulse: 0.3,
+  autojumpCooldown: 10, // ticks (0.5s)
+  bubbleColumnSurfaceDrag: {
+    down: 0.03,
+    maxDown: -0.9,
+    up: 0.1,
+    maxUp: 1.8
+  },
+  bubbleColumnDrag: {
+    down: 0.03,
+    maxDown: -0.3,
+    up: 0.06,
+    maxUp: 0.7
+  },
+  slowFalling: 0.125,
+  speedEffect: 1.2,
+  slowEffect: 0.85,
+  canEntityCollide: true
+}
+
+function getPlayerBB (pos) {
+  const w = physics.playerHalfWidth
+  return new AABB(-w, 0, -w, w, physics.playerHeight, w).offset(pos.x, pos.y, pos.z)
+}
+
 function makeSupportFeature (mcData) {
   return feature => features.some(({ name, versions }) => name === feature && versions.includes(mcData.version.majorVersion))
 }
@@ -40,58 +87,12 @@ function Physics (mcData, world) {
   const bubblecolumnId = blocksByName.bubble_column ? blocksByName.bubble_column.id : -1 // 1.13+
   if (blocksByName.bubble_column) waterLike.add(bubblecolumnId)
 
-  const physics = {
-    gravity: 0.08, // blocks/tick^2 https://minecraft.gamepedia.com/Entity#Motion_of_entities
-    airdrag: Math.fround(1 - 0.02), // actually (1 - drag)
-    yawSpeed: 3.0,
-    pitchSpeed: 3.0,
-    sprintSpeed: 1.3,
-    sneakSpeed: 0.3,
-    stepHeight: 0.6, // how much height can the bot step on without jump
-    negligeableVelocity: 0.003, // actually 0.005 for 1.8, but seems fine
-    soulsandSpeed: 0.4,
-    honeyblockSpeed: 0.4,
-    honeyblockJumpSpeed: 0.4,
-    ladderMaxSpeed: 0.15,
-    ladderClimbSpeed: 0.2,
-    playerHalfWidth: 0.3,
-    playerHeight: 1.8,
-    waterInertia: 0.8,
-    lavaInertia: 0.5,
-    liquidAcceleration: 0.02,
-    airborneInertia: 0.91,
-    airborneAcceleration: 0.02,
-    defaultSlipperiness: 0.6,
-    outOfLiquidImpulse: 0.3,
-    autojumpCooldown: 10, // ticks (0.5s)
-    bubbleColumnSurfaceDrag: {
-      down: 0.03,
-      maxDown: -0.9,
-      up: 0.1,
-      maxUp: 1.8
-    },
-    bubbleColumnDrag: {
-      down: 0.03,
-      maxDown: -0.3,
-      up: 0.06,
-      maxUp: 0.7
-    },
-    slowFalling: 0.125,
-    speedEffect: 1.2,
-    slowEffect: 0.85
-  }
-
   if (supportFeature('independentLiquidGravity')) {
     physics.waterGravity = 0.02
     physics.lavaGravity = 0.02
   } else if (supportFeature('proportionalLiquidGravity')) {
     physics.waterGravity = physics.gravity / 16
     physics.lavaGravity = physics.gravity / 4
-  }
-
-  function getPlayerBB (pos) {
-    const w = physics.playerHalfWidth
-    return new AABB(-w, 0, -w, w, physics.playerHeight, w).offset(pos.x, pos.y, pos.z)
   }
 
   function setPositionToBB (bb, pos) {
@@ -538,6 +539,36 @@ function Physics (mcData, world) {
     return isInWater
   }
 
+  function getEntityCollision (theEntity, otherEntity) {
+    // This should always pass, physics tick shouldn't be emitted when bot is on a vehicle & players can't be ridden on
+    if (theEntity.vehicle !== otherEntity && otherEntity.vehicle !== theEntity) {
+      // Need to set noclip flag somewhere (?)
+      if (!theEntity.noClip && !otherEntity.noClip) {
+        const pos1 = theEntity.position
+        const pos2 = otherEntity.position
+
+        let xDist = pos1.x - pos2.x
+        let zDist = pos1.z - pos2.z
+        let maxDist = math.abs_max(xDist, zDist)
+
+        if (maxDist >= 0.01) {
+          maxDist = Math.sqrt(maxDist) // Don't know how the following gets the right value but it works so...
+          xDist = xDist / maxDist
+          zDist = zDist / maxDist
+
+          const multiplier = Math.min(1 / maxDist, 1)
+          xDist = xDist * multiplier * 0.05
+          zDist = zDist * multiplier * 0.05
+          // xDist = xDist * (1 - bot.entity.entityCollisionReduction) // Couldn't find any references, left in here commented because forge has it
+          // zDist = zDist * (1 - bot.entity.entityCollisionReduction)
+
+          return new Vec3(xDist, 0, zDist)
+        }
+      }
+    }
+    return new Vec3(0, 0, 0)
+  }
+
   physics.simulatePlayer = (entity, world) => {
     const vel = entity.vel
     const pos = entity.pos
@@ -552,6 +583,12 @@ function Physics (mcData, world) {
     if (Math.abs(vel.x) < physics.negligeableVelocity) vel.x = 0
     if (Math.abs(vel.y) < physics.negligeableVelocity) vel.y = 0
     if (Math.abs(vel.z) < physics.negligeableVelocity) vel.z = 0
+
+    // Assuming entity = PlayerState
+    const intersectingEntities = entity.intersectingEntities
+    for (let i = 0; i < intersectingEntities.length; i++) {
+      vel.add(getEntityCollision(entity.bot.entity, intersectingEntities[i]))
+    }
 
     // Handle inputs
     if (entity.control.jump || entity.jumpQueued) {
@@ -644,6 +681,29 @@ function getStatusEffectNamesForVersion (supportFeature) {
   }
 }
 
+function getEntityBB (entity) {
+  const w = entity.width / 2
+  const pos = entity.position
+
+  return new AABB(-w, 0, -w, w, entity.height, w).offset(pos.x, pos.y, pos.z)
+}
+
+function getIntersectingEntities (bb, entityList) {
+  const entities = []
+
+  for (let i = 0; i < entityList.length; i++) {
+    const e = entityList[i]
+    if (e.type !== 'object') {
+      const entityBB = getEntityBB(e)
+      if (bb.intersects(entityBB)) {
+        entities.push(e)
+      }
+    }
+  }
+
+  return entities
+}
+
 class PlayerState {
   constructor (bot, control) {
     const mcData = require('minecraft-data')(bot.version)
@@ -651,6 +711,7 @@ class PlayerState {
     const supportFeature = makeSupportFeature(mcData)
 
     // Input / Outputs
+    this.bot = bot
     this.pos = bot.entity.position.clone()
     this.vel = bot.entity.velocity.clone()
     this.onGround = bot.entity.onGround
@@ -665,6 +726,8 @@ class PlayerState {
     // Input only (not modified)
     this.yaw = bot.entity.yaw
     this.control = control
+    // Hardcoded AABB because can't access getPlayerBB()
+    this.intersectingEntities = supportFeature('entityCollision') && physics.canEntityCollide ? getIntersectingEntities(getPlayerBB(this.pos), Object.values(bot.entities ?? {}).filter((e) => e.id !== bot.entity.id)) : []
 
     // effects
     const effects = bot.entity.effects
